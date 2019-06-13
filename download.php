499833ca534972b54db988ef958c095c4370d168
KUPASH <alexshapuk@yandex.ru>
2019-06-13 16:05:45 +0300

    first commit

---
+++ download.php
@@ -0,0 +1,28 @@
+<?php
+ini_set('display_errors', true);
+ini_set('display_startup_errors', true);
+error_reporting(E_ALL);
+
+if(isset($_GET['name'])) {
+    $name = $_GET['name'];
+    if(file_exists('./uploads/' .
+        $name[0] . '/' .
+        $name[1]) . '/' .
+        $name) {
+        $path = './uploads/' .
+            $name[0] . '/' .
+            $name[1] . '/' .
+            $name;
+
+        header('Content-Description: File Transfer');
+        header('Content-Type: octet-stream');
+        header('Content-Disposition: attachment; filename=' . basename($path));
+        header('Content-Transfer-Encoding: binary');
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate');
+        header('Pragma: public');
+        header('Content-Length: ' . filesize($path));
+        readfile($path);
+    }
+}
+header('Location: index.html');
---
+++ index.php
@@ -0,0 +1,81 @@
+<?php
+ini_set('display_errors', true);
+ini_set('display_startup_errors', true);
+error_reporting(E_ALL);
+?>
+<html>
+<head>
+    <meta charset="UTF-8">
+</head>
+<body>
+    <form method="post" enctype="multipart/form-data">
+        <input type="file" name="userFile">
+        <button type="submit">Submit</button>
+    </form>
+    Users have previously submitted:
+
+    <?
+        $allowed_ext = ['jpg', 'jpeg'];
+
+        echo '<pre>';
+        print_r($_FILES);
+        echo '</pre>';
+
+        if(isset($_FILES['userFile'])) {
+            $filename = md5(time() . rand(1, 9999) . $_FILES['userFile']['name']);
+
+            $ext = explode('.', $_FILES['userFile']['name']);
+            $ext = $ext[count($ext) - 1];
+
+            if (!in_array($ext, $allowed_ext)) {
+                exit();
+            }
+        }
+        if(isset($filename[0]) && isset($filename[1])) {
+            $subdirname1 = $filename[0];
+            $subdirname2 = $filename[1];
+
+            if (!file_exists('./uploads/' .
+                $subdirname1 . '/' .
+                $subdirname2)
+            ) {
+                mkdir('./uploads/' .
+                    $subdirname1 . '/' .
+                    $subdirname2, 0777, true);
+            }
+
+            move_uploaded_file($_FILES['userFile']['tmp_name'],
+                './uploads/' .
+                $subdirname1 . '/' .
+                $subdirname2 . '/' .
+                $filename . '.' . $ext);
+        }
+        function dirToArray($dir) {
+
+            $result = [];
+
+            $cdir = scandir($dir);
+            foreach ($cdir as $value) {
+                if (!in_array($value,array(".",".."))) {
+                    if (is_dir($dir . DIRECTORY_SEPARATOR . $value))
+                    {
+                        $result[$value] = dirToArray($dir . DIRECTORY_SEPARATOR . $value);
+                    }
+                    else
+                    {
+                        $result[] = $value;
+                        echo '<img src="'. $dir . DIRECTORY_SEPARATOR . $value .'" width="100">';
+                        echo '<div><a href="download.php?name=' . $value . '">Link</a><div>';
+                    }
+                }
+            }
+
+            return $result;
+
+        }
+
+        dirToArray('./uploads/');
+
+        ?>
+</body>
+</html>
